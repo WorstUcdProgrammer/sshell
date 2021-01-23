a191fb3fbfb55b51ac6e64888d158baa51b82244
WorstUcdProgrammer <50061136+WorstUcdProgrammer@users.noreply.github.com>
2021-01-22 22:42:43 -0800

    v1.4

--- sshell.c
+++ sshell.c
@@ -37,12 +37,12 @@ enum parsing_state {
 
 struct command {
         char *argv[17];
-        struct command *next_command;
+        struct command * next_command;
         char read_from[CMDLINE_MAX];
         char write_to[CMDLINE_MAX];
 };
 
-
+/* find the first occurance of a character inside a string */
 int first_index(char check, char cmd[CMDLINE_MAX]) {
         int index = -1;
         for (int i = 0; cmd[i] != '\0'; i++) {
@@ -54,14 +54,14 @@ int first_index(char check, char cmd[CMDLINE_MAX]) {
         return index;
 }
 
-
+/* implement the substring function */
 void substring(char original[CMDLINE_MAX], char substring[CMDLINE_MAX], int start, int end)
 {
         memcpy(substring, &original[start], end-start);
         substring[end-start] = '\0';
 }
 
-
+/* count the occurance of a character inside a string */
 int count_char(char check, char cmd[CMDLINE_MAX]) {
         int counter = 0;
         for (int i = 0; cmd[i] != '\0'; i++) {
@@ -72,7 +72,7 @@ int count_char(char check, char cmd[CMDLINE_MAX]) {
         return counter;
 }
 
-
+/* compute the total length of the command */
 int command_len(char cmd[CMDLINE_MAX]) {
         int len = 0;
         for (int i = 0; cmd[i] != '\0'; i++) {
@@ -89,6 +89,7 @@ int parse_command(struct command *obj, char cmd[CMDLINE_MAX]) {
 
         struct command *current_command = obj;
 
+        /* Case1: simple command with no pipe or output redirect */
         if (strchr(cmd, pipe) == NULL && strchr(cmd, redirect) == NULL) {
         
                 int counter = 0;
@@ -107,10 +108,8 @@ int parse_command(struct command *obj, char cmd[CMDLINE_MAX]) {
                 }
                 current_command->argv[counter] = NULL;
                 return NO_REDIRECT_NO_PIPE;
-        }
-
-        if (strchr(cmd, redirect) != NULL) {
-
+        } else if (strchr(cmd, redirect) != NULL && strchr(cmd, pipe) == NULL) {
+                /* Case2: command with output redirect but no pipe */
                 int redirect_sign = first_index('>', cmd);
                 char command_before[CMDLINE_MAX];
                 char dest_after[CMDLINE_MAX];
@@ -119,11 +118,11 @@ int parse_command(struct command *obj, char cmd[CMDLINE_MAX]) {
                 command_length = command_len(cmd);
                 substring(cmd, command_before, start, redirect_sign);
                 substring(cmd, dest_after, redirect_sign+1, command_length);
-
+/* misclosed redirection
                 if (count_char('|', dest_after)) {
                         return ERR_MISLOCATED_RED;
                 }
-
+*/
                 char * dest_after_no_space;
                 char * command_before_no_space;
                 dest_after_no_space = strtok(dest_after, " ");
@@ -143,9 +142,11 @@ int parse_command(struct command *obj, char cmd[CMDLINE_MAX]) {
                                         if (counter == 14) {
                                                 return ERR_TOOMANY_ARG;
                                         }
+                                        
                                         if (count_char('>', current_str)) {
                                                 break;
                                         }
+                                        
                                         current_command->argv[counter] = current_str;
                                         counter++;
                                         current_str = strtok(NULL, " ");
@@ -154,6 +155,84 @@ int parse_command(struct command *obj, char cmd[CMDLINE_MAX]) {
                                 strcpy(current_command->write_to, dest_after_no_space);
                                 return WITH_ONLY_REDIRECT;
                         }
+                }
+        } else {
+                int start = 0;
+                int seg_counter;
+                int pipe_count = count_char('|', cmd);
+                char parsing_string[CMDLINE_MAX];
+                strcpy(parsing_string, cmd);
+                for (seg_counter = 0; seg_counter < pipe_count; seg_counter++) {
+                        int pipe_sign = first_index('|', parsing_string);
+                        char command_before[CMDLINE_MAX];
+                        char rest_after[CMDLINE_MAX];
+                        int command_length = command_len(parsing_string);
+                        substring(parsing_string, command_before, start, pipe_sign);
+                        substring(parsing_string, rest_after, pipe_sign+1, command_length);
+                        strcpy(parsing_string, rest_after);
+                        if (count_char('>', command_before)) {
+                                return ERR_MISLOCATED_RED;
+                        }
+                        //printf("first half: %s\n", command_before);
+                        //printf("second half: %s\n", rest_after);
+                        int counter = 0;
+                        char * current_str;
+                        current_str = strtok(command_before, " ");
+                        while (current_str != NULL) {
+                                current_command->argv[counter] = current_str;
+                                counter++;
+                                current_str = strtok(NULL, " ");
+                        }
+                        current_command->argv[counter] = NULL;
+                        strcpy(parsing_string, rest_after);
+                        current_command = current_command->next_command;
+                }
+
+                if (count_char('>', parsing_string)) {
+                        int redirect_sign = first_index('>', parsing_string);
+                        char command_before[CMDLINE_MAX];
+                        char dest_after[CMDLINE_MAX];
+                        int start = 0;
+                        int command_length;
+                        command_length = command_len(parsing_string);
+                        substring(parsing_string, command_before, start, redirect_sign);
+                        substring(parsing_string, dest_after, redirect_sign+1, command_length);
+                        char * dest_after_no_space;
+                        dest_after_no_space = strtok(dest_after, " ");
+                        if (dest_after_no_space == NULL || command_before == NULL) {
+                                return ERR_MISSING_CMD;
+                        } else {
+                                if (!access(dest_after_no_space, F_OK) && access(dest_after_no_space, W_OK)) {
+                                        return ERR_CANT_OPEN;
+                                } else {
+                                        int counter = 0;
+                                        char * current_str;
+                                        current_str = strtok(command_before, " ");
+                                        while (current_str != NULL) {
+                                                /*
+                                                if (count_char('>', current_str)) {
+                                                        break;
+                                                }
+                                                */
+                                                //printf("%s\n", current_str);
+                                                current_command->argv[counter] = current_str;
+                                                counter++;
+                                                current_str = strtok(NULL, " ");
+                                        }
+                                        current_command->argv[counter] = NULL;
+                                        strcpy(current_command->write_to, dest_after_no_space);
+                                }
+                        }
+                } else {
+                        int counter = 0;
+                        char * current_str;
+                        current_str = strtok(parsing_string, " ");
+                        while (current_str != NULL) {
+                                current_command->argv[counter] = current_str;
+                                counter++;
+                                current_str = strtok(NULL, " ");
+                        }
+                        current_command->argv[counter] = NULL;
                 }
         }
         return SUCCESS_FLAG;
@@ -251,9 +330,12 @@ int main(void)
                 struct command command_obj_two;
                 struct command command_obj_three;
                 struct command command_obj_four;
-                command_obj_one.next_command = &command_obj_two;
-                command_obj_two.next_command = &command_obj_three;
-                command_obj_three.next_command = &command_obj_four;
+                struct command *command_obj_two_ptr = &command_obj_two;
+                struct command *command_obj_three_ptr = &command_obj_three;
+                struct command *command_obj_four_ptr = &command_obj_four;
+                command_obj_one.next_command = command_obj_two_ptr;
+                command_obj_two.next_command = command_obj_three_ptr;
+                command_obj_three.next_command = command_obj_four_ptr;
                 char command[CMDLINE_MAX];
                 strcpy(command, cmd);
                 int return_state = parse_command(&command_obj_one, command);
@@ -332,13 +414,58 @@ int main(void)
                                 int fd1;
                                 fd1 = open(command_obj_one.write_to, O_WRONLY | O_CREAT, 0644);
                                 dup2(fd1, STDOUT_FILENO);
+                                close(fd1);
 
                                 if (execvp(command_obj_one.argv[0], command_obj_one.argv) == -1) {
                                         exit(1);
-                                        close(fd1);
                                 }
                                 exit(0);
-                                close(fd1);
+                        }
+                }
+
+                if (return_state == SUCCESS_FLAG) {
+                        if(count_char('|', cmd) == 1) {
+                                int fd[2];
+                                int status_one;
+                                pipe(fd);
+                                int pid = fork();
+                                if (pid != 0) {
+                                        wait(&status_one);
+                                        if (status_one != 0) {
+                                              error_message(ERR_CMD_NOTFOUND);
+                                              continue;  
+                                        }
+                                } else {
+                                        close(fd[0]);
+                                        dup2(fd[1], STDOUT_FILENO);
+                                        close(fd[1]);
+                                        if (execvp(command_obj_one.argv[0], command_obj_one.argv) == -1) {
+                                                exit(1);
+                                        }
+                                        exit(0);
+                                }
+
+                                pipe(fd);
+                                pid = fork();
+                                if (pid != 0) {
+                                        int status;
+                                        wait(&status);
+                                        if (status != 0) {
+                                              error_message(ERR_CMD_NOTFOUND);
+                                              continue;  
+                                        }
+                                        fprintf(stderr,
+                                                "+ completed '%s' [%d][%d]\n",
+                                                cmd, status_one, status);
+                                } else {
+                                        close(fd[1]);
+                                        dup2(fd[0], STDIN_FILENO);
+                                        close(fd[0]);
+                                        if (execvp(command_obj_two.argv[0], command_obj_two.argv) == -1) {
+                                                exit(1);
+                                        }
+                                        exit(0);
+                                }
                         }
                 }
         }
